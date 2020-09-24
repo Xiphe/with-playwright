4db8bc7b9c49d623efe9c8c87e634a185a329790
Xiphe <github@xiphe.net>
2020-09-24 13:42:56 +0200

    feat: export getBrowsers helper

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { LaunchedPage as L } from './getLaunchedPage';
 import { PlaywrightBrowser as PWB, BrowserConfig as BC } from './getBrowsers';
 
 export { default } from './withPlaywright';
+export { getBrowsers } from './getBrowsers';
 export type LaunchedPage = L;
 export type PlaywrightBrowser = PWB;
 export type BrowserConfig = BC;
