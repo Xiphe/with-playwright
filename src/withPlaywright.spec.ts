28e96a50718d0a7576432705612317049df9c95a
Hannes Diercks <github@xiphe.net>
2021-11-25 19:00:25 +0100

    chore: semantic-release
    * test: test other browsers
    
    * fix: auto-release
    
    * chore: fix release config
    
    * chore: pre-install semantic-release
    
    * chore: update dev deps
    
    * chore: how about node@14?
    
    * chore: common release dis!
    
    * chore: fix ci
    
    * chore: release-config

--- src/withPlaywright.spec.ts
+++ src/withPlaywright.spec.ts
@@ -3,6 +3,7 @@ import { Page } from 'playwright-core/lib/client/page';
 import { Browser } from 'playwright-core/lib/client/browser';
 import { LaunchedPage } from './getLaunchedPage';
 import withPlaywright from './withPlaywright';
+import { AVAILABLE_PLAYWRIGHT_BROWSERS } from './getBrowsers';
 
 if (typeof global.setImmediate === 'undefined') {
   global.setImmediate = ((cb: any) => {
@@ -17,17 +18,20 @@ describe('withPlaywright', () => {
     expect(cb).not.toHaveBeenCalled();
   });
 
-  it('provides browser, context, page and name to callback', async () => {
-    const cb = jest.fn();
-    withPlaywright(cb, { browsers: ['chromium'] });
+  it.each(AVAILABLE_PLAYWRIGHT_BROWSERS)(
+    'provides browser, context, page and name to callback in %s',
+    async () => {
+      const cb = jest.fn();
+      withPlaywright(cb, { browsers: ['chromium'] });
 
-    expect(cb).toHaveBeenCalledTimes(1);
+      expect(cb).toHaveBeenCalledTimes(1);
 
-    const [name, pw]: [string, Promise<LaunchedPage>] = cb.mock.calls[0];
-    expect(name).toBe('chromium');
-    const { browser, context, page } = await pw;
-    expect(context).toEqual(expect.any(BrowserContext));
-    expect(browser).toEqual(expect.any(Browser));
-    expect(page).toEqual(expect.any(Page));
-  });
+      const [name, pw]: [string, Promise<LaunchedPage>] = cb.mock.calls[0];
+      expect(name).toBe('chromium');
+      const { browser, context, page } = await pw;
+      expect(context).toEqual(expect.any(BrowserContext));
+      expect(browser).toEqual(expect.any(Browser));
+      expect(page).toEqual(expect.any(Page));
+    },
+  );
 });
